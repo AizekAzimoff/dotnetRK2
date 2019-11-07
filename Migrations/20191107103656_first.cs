b9992090340965e05fb1c00849fad5dc12175dc1
AizekAzimoff <47439595+AizekAzimoff@users.noreply.github.com>
2019-11-07 16:40:46 +0600

    Remade them all

---
+++ Migrations/20191107103656_first.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace midterm.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    course_lang = table.Column<string>(nullable: true),
+                    course_dur = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    group_name = table.Column<string>(nullable: true),
+                    group_level = table.Column<string>(nullable: true),
+                    group_stud_num = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    RoomID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    room_number = table.Column<int>(nullable: false),
+                    room_seat_num = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.RoomID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeacherID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    teach_name = table.Column<string>(nullable: true),
+                    teach_phone = table.Column<int>(nullable: false),
+                    teach_mail = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeacherID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    stud_name = table.Column<string>(nullable: true),
+                    stud_phone = table.Column<int>(nullable: false),
+                    stud_mail = table.Column<int>(nullable: false),
+                    GroupID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentID);
+                    table.ForeignKey(
+                        name: "FK_Students_Groups_GroupID",
+                        column: x => x.GroupID,
+                        principalTable: "Groups",
+                        principalColumn: "GroupID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Abilities",
+                columns: table => new
+                {
+                    AbilityID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CourseID = table.Column<int>(nullable: false),
+                    TeacherID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abilities", x => x.AbilityID);
+                    table.ForeignKey(
+                        name: "FK_Abilities_Courses_CourseID",
+                        column: x => x.CourseID,
+                        principalTable: "Courses",
+                        principalColumn: "CourseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Abilities_Teachers_TeacherID",
+                        column: x => x.TeacherID,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    ScheduleID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    sch_day = table.Column<int>(nullable: false),
+                    RoomID = table.Column<int>(nullable: false),
+                    TeacherID = table.Column<int>(nullable: false),
+                    GroupID = table.Column<int>(nullable: false),
+                    CourseID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.ScheduleID);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Courses_CourseID",
+                        column: x => x.CourseID,
+                        principalTable: "Courses",
+                        principalColumn: "CourseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Groups_GroupID",
+                        column: x => x.GroupID,
+                        principalTable: "Groups",
+                        principalColumn: "GroupID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Rooms_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Teachers_TeacherID",
+                        column: x => x.TeacherID,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Groups",
+                columns: new[] { "GroupID", "group_level", "group_name", "group_stud_num" },
+                values: new object[] { -1, "Advanced", "English1602", 10 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Abilities_CourseID",
+                table: "Abilities",
+                column: "CourseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Abilities_TeacherID",
+                table: "Abilities",
+                column: "TeacherID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_CourseID",
+                table: "Schedules",
+                column: "CourseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_GroupID",
+                table: "Schedules",
+                column: "GroupID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_RoomID",
+                table: "Schedules",
+                column: "RoomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_TeacherID",
+                table: "Schedules",
+                column: "TeacherID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GroupID",
+                table: "Students",
+                column: "GroupID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abilities");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
