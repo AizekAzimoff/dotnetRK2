7a5690572d001a1e4a7b7053e5f9c4fb1ed410f9
AizekAzimoff <47439595+AizekAzimoff@users.noreply.github.com>
2019-11-07 15:40:08 +0600

    part 1

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using midterm.Data;
+
+namespace midterm
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Context>(options =>
+            {
+                options.UseSqlite("Filename=movies.db");
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Movies}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+        }
+    }
+}
