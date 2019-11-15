c3d22a22301cc5764e5512f577f08cee2c1c590d
AizekAzimoff <47439595+AizekAzimoff@users.noreply.github.com>
2019-11-16 05:48:43 +0600

    tests

---
+++ midtermTest/GroupTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using midterm.Models;
+using midterm.Services;
+using Moq;
+using Xunit;
+
+namespace midtermTest
+{
+    public class GroupTest
+    {
+
+
+        [Fact]
+        public void CreateTest()
+        {
+            String str = "1/CSSE1602//16";
+            String[] str_arr = str.Split('/');
+            Boolean check = true;
+            int id1 = Int32.Parse(str_arr[0]);
+            String group_name1 = str_arr[1];
+            String group_level1= str_arr[2];
+            int stud_num1 = Int32.Parse(str_arr[3]);
+            try{
+                Group group = new Group(id1, group_name1, group_level1, stud_num1);
+            }
+            catch{
+                check = false;
+            }
+            Assert.True(check);
+        }
+
+        [Fact]
+        public void IsNullNameTest()
+        {
+            String name = null;
+            Assert.Null(name);
+        }
+
+        [Fact]
+        public void IsEmptyNameTest()
+        {
+            String name = "s";
+            Assert.NotEmpty(name);
+        }
+
+        [Fact]
+        public void ForbiddenSymbolsInNameTest()
+        {
+            String str = "Aziz", str2 = "!@#$%^&*()_+-=";
+            Boolean check = true;
+            for (int i = 0; i < str.Length; i++){
+                for (int j = 0; j < str2.Length; j++){
+                    if (str[i] == str2[j]){
+                        check = false;
+                    }
+                }
+            }
+            Assert.True(check);
+        }
+
+        [Fact]
+        public void IsNullLevelTest()
+        {
+            String lvl = null;
+            Assert.Null(lvl);
+        }
+
+        [Fact]
+        public void IsEmptyLevelTest()
+        {
+            String lvl = "s";
+            Assert.NotEmpty(lvl);
+        }
+        public void NotNullIdTest()
+        {
+            int? id = 5;
+            Boolean check = true;
+            if (id == null) check = false;
+            Assert.True(check);
+        }
+
+        [Fact]
+        public void IsNegativeIdTest()
+        {
+            int id = 5;
+            Boolean check = true;
+            if (id < 0) check = false;
+            Assert.True(check);
+        }
+    }
+}
---
+++ midtermTest/GroupsServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using midterm.Services;
+using Moq;
+using Xunit;
+namespace midterm.Models
+{
+    public class GroupsServiceTests
+    {
+
+        [Fact]
+        public async Task AddTest()
+        {
+            var fakeRepository = Mock.Of<IGroupsRepository>();
+            var groupService = new GroupService(fakeRepository);
+
+            var group = new Group() { group_name = "test group" };
+            await groupService.AddAndSave(group);
+        }
+        [Fact]
+        public async Task NullNameAddTest()
+        {
+            var fakeRepository = Mock.Of<IGroupsRepository>();
+            var groupService = new GroupService(fakeRepository);
+            String name = "";
+            Assert.NotNull(name);
+            var group = new Group() { group_name = name };
+            await groupService.AddAndSave(group);
+        }
+
+        [Fact]
+        public async Task GetGroupsTest()
+        {
+            var groups = new List<Group>
+            {
+                new Group() { group_name = "test group 1" },
+                new Group() { group_name = "test group 2" },
+            };
+            var fakeRepositoryMock = new Mock<IGroupsRepository>();
+            fakeRepositoryMock.Setup(x => x.GetAll()).ReturnsAsync(groups);
+
+
+            var groupService = new GroupService(fakeRepositoryMock.Object);
+
+            var resultGroups = await groupService.GetGroups();
+
+            Assert.Collection(resultGroups, group =>
+            {
+                Assert.Equal("test group 1", group.group_name);
+            },
+            group =>
+            {
+                Assert.Equal("test group 2", group.group_name);
+            });
+        }
+
+
+
+
+        [Fact]
+        public async Task editGroupTest()
+        {
+
+            var groups = new Group() { GroupID = 1 };
+
+            var fakeRepository = Mock.Of<IGroupsRepository>();
+            var groupService = new GroupService(fakeRepository);
+
+            var resultGroups = await groupService.GetGroupToEdit(1);
+            
+                Assert.Equal(resultGroups.GroupID, groups.GroupID);
+          
+
+            
+            
+        }
+
+    }
+}
